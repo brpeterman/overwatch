1e9c21f709c9ae036da867f3d873f6c0324d35e9
Brandon Peterman <brpeterman@gmail.com>
2015-06-08 23:21:06 -0500

    Delay the first data report to give the daemon a chance to wake up.

--- public/refresh.rb
+++ public/refresh.rb
@@ -29,25 +29,30 @@ $connected = true
 last_update = nil
 while $connected do
   begin
+    # If nobody has touched the tickler in a while, skip an iteration so that the data can be refreshed
+    skip_once = (File::mtime('../tickler').to_i < Time.now.to_i - 5*60)
+
     # Notify the daemon that we're interested in an update
     FileUtils.touch "../tickler"
 
-    File.open("../status.json", "r") do |file|
-      file.flock(File::LOCK_SH) # block until file is available (shouldn't be long)
-      $have_lock = true
-      # Check if there has been a change since we last read the status
-      current_update = File.mtime(file.path)
-      if current_update != last_update
-        # There shouldn't be multiple lines, but if there are, make sure they send as part of the same data message
-        status_data = file.readlines.join "\ndata: "
-        $stdout.print "data: #{status_data}\n\n"
-        $stdout.flush
-        last_update = current_update
+    if !skip_once
+      File.open("../status.json", "r") do |file|
+        file.flock(File::LOCK_SH) # block until file is available (shouldn't be long)
+        $have_lock = true
+        # Check if there has been a change since we last read the status
+        current_update = File.mtime(file.path)
+        if current_update != last_update
+          # There shouldn't be multiple lines, but if there are, make sure they send as part of the same data message
+          status_data = file.readlines.join "\ndata: "
+          $stdout.print "data: #{status_data}\n\n"
+          $stdout.flush
+          last_update = current_update
+        end
+        file.flock(File::LOCK_UN)
+        $have_lock = false
       end
-      file.flock(File::LOCK_UN)
-      $have_lock = false
     end
-    sleep 10
+    sleep 3
   rescue Exception => e
     # If we fail to write to the stream, that means it's closed and we need to stop looping
     $stderr.puts e.inspect
