fd428b4733c22dec7f7e62022e5e010bfa40e926
Brandon Peterman <brpeterman@gmail.com>
2015-07-16 19:05:49 -0500

    Fixed #1: Moved entirely away from the refresh loop. Data is now generated on demand only.

--- status-daemon.rb
+++ status-daemon.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-#TODO: Build infrastructure to sleep and wake up on demand, keep track of when we were last asked to wake up
-
 require_relative 'server-status'
 require 'json'
 require 'drb/drb'
@@ -15,13 +13,10 @@ def run!
   $daemon = Overwatch::StatusDaemon.new
   
   DRb.start_service(uri, $daemon)
-  $daemon.start # this line blocks
-
   DRb.thread.join if DRb.thread
 end
 
 def terminate
-  $daemon.stop
   DRb.stop_service
 end
 
@@ -36,6 +31,14 @@ module Overwatch
       @last_update = nil
       @last_request = Time.now
       @server_status = Overwatch::ServerStatus.new nil, true
+
+      @servers = []
+      @servers << 'minecraft'
+      @servers << 'starbound'
+      @servers << 'kerbal'
+      @servers << 'sevendays'
+      @servers << 'mumble'
+      @servers << 'terraria'
     end
 
     # Tries to call a method on the server status object.
@@ -46,66 +49,32 @@ module Overwatch
       end
     end
 
-    def start
-      servers = []
-      servers << 'minecraft'
-      servers << 'starbound'
-      servers << 'kerbal'
-      servers << 'sevendays'
-      servers << 'mumble'
-      servers << 'terraria'
-
-      @do_query = true
-      last_state = :sleep
-      state = :sleep
-
-      while @do_query do
-        # Check if anybody actually wants an update
-        if Time.now.to_i - @last_request.to_i < 2*60
-          last_state = state
-          state = :awake
-          $stderr.puts "[#{Time.now}] Sending queries"
-          begin
-            servers.each do |type|
-              break unless @do_query
-              @server_status.send("#{type}_reinitialize") # re-ping the server
-              @status[type] = {}
-              @status[type]['online'] = try_method("#{type}_status")
-              @status[type]['player count']  = try_method("#{type}_player_count")
-              @status[type]['motd'] = try_method("#{type}_motd")
-              @status[type]['player list'] = try_method("#{type}_player_list")
-            end
-
-            # All sorts of invalid input can potentially cause an error. Whatever it is, just make sure we return a valid object.
-          rescue Exception => e
-            $stderr.puts e.inspect
-            $stderr.puts e.backtrace
-            @status = {}
-          end
-
-        else # Nobody wants an update, so go to sleep
-          last_state = state
-          state = :sleep
+    def update_status
+      begin
+        $stderr.puts "[#{Time.now}] Sending queries"
+        @servers.each do |type|
+          @server_status.send("#{type}_reinitialize") # re-ping the server
+          @status[type] = {}
+          @status[type]['online'] = try_method("#{type}_status")
+          @status[type]['player count']  = try_method("#{type}_player_count")
+          @status[type]['motd'] = try_method("#{type}_motd")
+          @status[type]['player list'] = try_method("#{type}_player_list")
         end
 
-        if state != last_state and state == :sleep
-          $stderr.puts "[#{Time.now}] Going to sleep"
-        end
-
-        # Hacky way to sleep for a while but still terminate reasonably fast after getting a signal
-        10.times do
-          break unless @do_query
-          sleep 1
-        end
+        # All sorts of invalid input can potentially cause an error. Whatever it is, just make sure we return a valid object.
+      rescue Exception => e
+        $stderr.puts e.inspect
+        $stderr.puts e.backtrace
+        @status = {}
       end
     end
 
-    def stop
-      @do_query = false
-    end
-
     def status
-      @last_request = Time.now
+      # If the data is over 10 seconds old, refresh it
+      if !@last_update or (Time.now.to_i - @last_update.to_i > 10)
+        update_status
+        @last_update = Time.now
+      end
       @status
     end
   end
