304c6442d0a749f1c761dbeaa9197257c357d1c4
Brandon Peterman <brpeterman@gmail.com>
2015-07-21 14:35:25 -0500

    Refactoring, no functional changes.

--- index.rb
+++ index.rb
@@ -12,19 +12,29 @@ def build_html(sections)
 
   # Build the tabs that show server status
   # Build the details section for each server
+  tabs_html, sections_html = build_sections_html(cgi, status, sections)
+  all_html = build_wrapping_html(cgi, tabs_html, sections_html)
+
+  # Output the HTML we just built
+  cgi.out do
+    CGI::pretty(all_html)
+  end
+end
+
+def build_sections_html(cgi, status, sections)
   tabs_html, sections_html = "", ""
   sections.each do |type, title|
-    tabs_html += cgi.div({'id' => "#{type}-status",
-                      'class' => 'statusline',
-                      'onclick' => "selectTab('#{type}')"}) do
+    tabs_html += cgi.div('id' => "#{type}-status",
+                         'class' => 'statusline',
+                         'onclick' => "selectTab('#{type}')") do
       title +
       ( status.respond_to?("#{type}_player_count") ?
-        cgi.span({'class' => 'player-count'}) do
+        cgi.span('class' => 'player-count') do
           "(...)"
         end
         : "" ) +
-      cgi.div({'class' => 'status-summary'}) do
-        cgi.span({'class' => 'status offline'}) do
+      cgi.div('class' => 'status-summary') do
+        cgi.span('class' => 'status offline') do
           "Loading"
         end
       end
@@ -32,28 +42,27 @@ def build_html(sections)
 
     sections_html += send("build_#{type}_section", cgi, status)
   end
+  [tabs_html, sections_html]
+end
 
-  # Output the HTML we just built
-  cgi.out do
-    CGI::pretty(cgi.html do
-      "\n\n<!-- This HTML is auto-generated. Please don't judge me on the formatting. :) -->\n\n" +
-      cgi.head do
-        cgi.title { "Overwatch" } +
-          cgi.link({'rel' => 'stylesheet',
-                     'href' => 'style.css'}) +
-          cgi.script({'type' => 'text/javascript',
-                       'src' => 'script.js'}) +
-          cgi.meta({'name' => 'viewport',
-                     'content' => 'width=device-width, initial-scale=1'}) + # Let mobile devices do their own scaling
-          cgi.meta({'charset' => 'utf-8'})
+def build_wrapping_html(cgi, tabs_html, sections_html)
+  cgi.html do
+    cgi.head do
+      cgi.title { "Overwatch" } +
+      cgi.link('rel' => 'stylesheet',
+               'href' => 'style.css') +
+      cgi.script('type' => 'text/javascript',
+                 'src' => 'script.js') +
+      cgi.meta('name' => 'viewport',
+               'content' => 'width=device-width, initial-scale=1') + # Let mobile devices do their own scaling
+      cgi.meta('charset' => 'utf-8')
+    end +
+    cgi.body do
+      cgi.div('id' => 'status-box') do
+        tabs_html
       end +
-      cgi.body do
-        cgi.div({'id' => 'status-box'}) do
-          tabs_html
-        end +
-        sections_html
-      end
-    end)
+      sections_html
+    end
   end
 end
 
@@ -66,23 +75,25 @@ end
 # cgi is the CGI object we're using for output
 # status is the ServerStatus object
 def build_minecraft_section(cgi, status)
-  cgi.div({'id' => 'minecraft-section',
-            'class' => 'section'}) do
+  cgi.div('id' => 'minecraft-section',
+          'class' => 'section') do
     details_section(cgi, 'minecraft') do
       details_line(cgi, "Address", status.minecraft_address, 'address') +
       details_line(cgi, "MOTD", status.minecraft_motd, 'motd') +
       details_line(cgi, "Map",
-                   cgi.a({'href' => 'http://mc.bpeterman.com:25766/'}) do
+                   cgi.a('href' => 'http://mc.bpeterman.com:25766/') do
                      "http://mc.bpeterman.com:25766/"
                    end, 'map')
     end + 
     # Map IFrame
-    cgi.div({'id' => 'map-container'}) do
-      cgi.span({'id' => 'toggle-map',
-                 'onclick' => 'toggleMap()'}) do
+    cgi.div('id' => 'map-container') do
+      cgi.span('id' => 'toggle-map',
+               'onclick' => 'toggleMap()') do
         "Show Map"
       end +
-      cgi.iframe({'id' => 'map-frame', 'seamless' => 'seamless', 'src' => 'http://mc.bpeterman.com:25766'})
+      cgi.iframe('id' => 'map-frame',
+                 'seamless' => 'seamless',
+                 'src' => 'http://mc.bpeterman.com:25766')
     end
   end
 end
@@ -91,8 +102,8 @@ end
 # cgi is the CGI object we're using for output
 # status is the ServerStatus object
 def build_starbound_section(cgi, status)
-  cgi.div({'id' => 'starbound-section',
-            'class' => 'section'}) do
+  cgi.div('id' => 'starbound-section',
+          'class' => 'section') do
     details_section(cgi, 'starbound') do
       details_line(cgi, "Address", status.starbound_address, 'address')
     end
@@ -103,8 +114,8 @@ end
 # cgi is the CGI object we're using for output
 # status is the ServerStatus object
 def build_kerbal_section(cgi, status)
-  cgi.div({'id' => 'kerbal-section',
-            'class' => 'section'}) do
+  cgi.div('id' => 'kerbal-section',
+          'class' => 'section') do
     details_section(cgi, 'kerbal') do
       details_line(cgi, "Address", status.kerbal_address, 'address') +
       details_line(cgi, "Players online", 
@@ -119,8 +130,8 @@ end
 # cgi is the CGI object we're using for output
 # status is the ServerStatus object
 def build_sevendays_section(cgi, status)
-  cgi.div({'id' => 'sevendays-section',
-            'class' => 'section'}) do
+  cgi.div('id' => 'sevendays-section',
+          'class' => 'section') do
     details_section(cgi, 'sevendays') do
       details_line(cgi, "Address", status.sevendays_address, 'address')
     end
@@ -131,8 +142,8 @@ end
 # cgi is the CGI object we're using for output
 # status is the ServerStatus object
 def build_mumble_section(cgi, status)
-  cgi.div({'id' => 'mumble-section',
-            'class' => 'section'}) do
+  cgi.div('id' => 'mumble-section',
+          'class' => 'section') do
     details_section(cgi, 'mumble') do
       details_line(cgi, "Address", status.mumble_address, 'address') +
       details_line(cgi, "Users online", 
@@ -147,8 +158,8 @@ end
 # cgi is the CGI object we're using for output
 # status is the ServerStatus object
 def build_terraria_section(cgi, status)
-  cgi.div({'id' => 'terraria-section',
-            'class' => 'section'}) do
+  cgi.div('id' => 'terraria-section',
+          'class' => 'section') do
     details_section(cgi, 'terraria') do
       details_line(cgi, "Address", status.terraria_address, 'address') +
       details_line(cgi, "Users online", 
@@ -166,11 +177,11 @@ end
 # type is the type of field. Used in the CSS class.
 def details_line(cgi, label, value, type)
   id = "#{type}-value"
-  cgi.div({'class' => 'details-line'}) do
-    cgi.span({'class' => 'details-label'}) do
+  cgi.div('class' => 'details-line') do
+    cgi.span('class' => 'details-label') do
       label
     end +
-    cgi.span({'class' => "details-value #{id}"}) do
+    cgi.span('class' => "details-value #{id}") do
       value
     end
   end
@@ -181,9 +192,9 @@ end
 # server_type is the type of server we're displaying (used in the element ID)
 # block is a block that evaluates to the content of the section
 def details_section(cgi, server_type, &block)
-  cgi.div({'id' => (server_type + '-details'),
-            'class' => 'details-section'}) do
-    cgi.div({'class' => 'header'}) do
+  cgi.div('id' => (server_type + '-details'),
+          'class' => 'details-section') do
+    cgi.div('class' => 'header') do
       "Server details"
     end +
     block.call
