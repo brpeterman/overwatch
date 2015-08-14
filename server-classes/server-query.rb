d2e58c20ec3422a47a805461849ebde8da8899f0
Brandon Peterman <brpeterman@gmail.com>
2015-08-13 20:18:52 -0500

    call -> send

--- server-classes/server-query.rb
+++ server-classes/server-query.rb
@@ -16,7 +16,7 @@ module Overwatch
       return {} if !defined? @info_providers
 
       @info_providers.reduce do |acc, method_name|
-        acc[method_name] = self.call(method_name)
+        acc[method_name] = self.send method_name
         acc
       end
     end
