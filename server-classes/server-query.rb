e04dcbb1985e05bea2657d0d4f86dcfb0e654c1c
Brandon Peterman <brpeterman@gmail.com>
2015-08-13 20:15:52 -0500

    For real this time.

--- server-classes/server-query.rb
+++ server-classes/server-query.rb
@@ -2,7 +2,7 @@ module Overwatch
   class ServerQuery
     def define_info(method_name, &block)
       if block != nil
-        self.class.send(:define_method) method_name, &block
+        self.class.send(:define_method, method_name, &block)
       end
 
       if !defined? @info_providers
