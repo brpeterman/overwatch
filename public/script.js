605dadeef24f488d7b74b010c623977f6b3f29a1
Brandon Peterman <brpeterman@gmail.com>
2015-08-13 20:22:53 -0500

    Fix crazy indentation.

--- public/script.js
+++ public/script.js
@@ -4,14 +4,14 @@
 function selectTab(tab) {
     var tabs = document.getElementsByClassName("statusline");
     for (var i = 0; i < tabs.length; i++) {
-	tabs[i].classList.remove("selected");
+	      tabs[i].classList.remove("selected");
     }
     var activeTab = document.getElementById(tab + "-status");
     activeTab.classList.add("selected");
 
     var sections = document.getElementsByClassName("section");
     for (var i = 0; i < sections.length; i++) {
-	sections[i].classList.remove("selected");
+	      sections[i].classList.remove("selected");
     }
     var activeSection = document.getElementById(tab + "-section");
     activeSection.classList.add("selected");
@@ -24,12 +24,12 @@ function toggleMap() {
     var mapElem = document.getElementById("map-frame");
     var linkElem = document.getElementById("toggle-map");
     if (mapElem.style.display == "block") {
-	mapElem.style.display = "none";
-	linkElem.innerHTML = "Show Map";
+	      mapElem.style.display = "none";
+	      linkElem.innerHTML = "Show Map";
     }
     else {
-	mapElem.style.display = "block";
-	linkElem.innerHTML = "Hide Map";
+	      mapElem.style.display = "block";
+	      linkElem.innerHTML = "Hide Map";
     }
 }
 
@@ -41,7 +41,7 @@ function resize() {
     var rect = sections[0].getBoundingClientRect();
     var browserHeight = document.documentElement.clientHeight;
     for (var i = 0; i < sections.length; i++) {
-	sections[i].style.height = (browserHeight - rect.top - 15) + "px";
+	      sections[i].style.height = (browserHeight - rect.top - 15) + "px";
     }
 }
 
@@ -53,43 +53,43 @@ function updateStatus(e) {
     if (!e.data) { return; }
     var status = JSON.parse(e.data);
     for (var key in status) {
-	var tabElem = document.getElementById(key + "-status")
-	if (tabElem) {
-	    var statusElem = tabElem.querySelector(".status")
-	    if (status[key]["online"]) {
-		statusElem.classList.remove("offline");
-		statusElem.classList.add("online");
-		statusElem.innerHTML = "Online";
-	    }
-	    else {
-		statusElem.classList.remove("online");
-		statusElem.classList.add("offline");
-		statusElem.innerHTML = "Offline";
-	    }
-	    var countElem = tabElem.querySelector(".player-count");
-	    if (countElem) {
-		countElem.innerHTML = "(" + status[key]["player count"] + ")";
-	    }
-	}
-	var serverStatus = status[key];
-	var sectionElem = document.getElementById(key + "-section");
-	if (sectionElem) {
-	    for (var field in serverStatus) {
-		var selector = "." + field.replace(' ', '_') + "-value";
-		var valueElem = sectionElem.querySelector(selector);
-		if (valueElem) {
-		    if (field == "player list") {
-			if (serverStatus[field] == "") {
-			    serverStatus[field] = "None";
-			}
-			else {
-			    serverStatus[field] = serverStatus[field].join(', ');
-			}
-		    }
-		    valueElem.innerHTML = serverStatus[field];
-		}
-	    }
-	}
+	      var tabElem = document.getElementById(key + "-status")
+	      if (tabElem) {
+	          var statusElem = tabElem.querySelector(".status")
+	          if (status[key]["online"]) {
+		            statusElem.classList.remove("offline");
+		            statusElem.classList.add("online");
+		            statusElem.innerHTML = "Online";
+	          }
+	          else {
+		            statusElem.classList.remove("online");
+		            statusElem.classList.add("offline");
+		            statusElem.innerHTML = "Offline";
+	          }
+	          var countElem = tabElem.querySelector(".player-count");
+	          if (countElem) {
+		            countElem.innerHTML = "(" + status[key]["player count"] + ")";
+	          }
+	      }
+	      var serverStatus = status[key];
+	      var sectionElem = document.getElementById(key + "-section");
+	      if (sectionElem) {
+	          for (var field in serverStatus) {
+		            var selector = "." + field.replace(' ', '_') + "-value";
+		            var valueElem = sectionElem.querySelector(selector);
+		            if (valueElem) {
+		                if (field == "player list") {
+			                  if (serverStatus[field] == "") {
+			                      serverStatus[field] = "None";
+			                  }
+			                  else {
+			                      serverStatus[field] = serverStatus[field].join(', ');
+			                  }
+		                }
+		                valueElem.innerHTML = serverStatus[field];
+		            }
+	          }
+	      }
     }
 }
 
