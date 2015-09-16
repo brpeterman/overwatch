686b68c790c7d04c73b407c0f0c1f3864326ee69
Brandon Peterman <brpeterman@gmail.com>
2015-09-16 12:57:25 -0500

    Documentation.

--- server-classes/server-query.rb
+++ server-classes/server-query.rb
@@ -1,5 +1,11 @@
 module Overwatch
+  # This is the parent class for all server query classes.
+  # It provides an interface for defining and retrieving discrete
+  # data about a server.
   class ServerQuery
+    # Define a method as one that provides info about a server.
+    # If [block] is passed, defines the method described by [block].
+    # Info-providing methods may not take any arguments.
     def define_info(method_name, &block)
       if block != nil
         self.class.send(:define_method, method_name, &block)
@@ -12,6 +18,9 @@ module Overwatch
       @info_providers << method_name
     end
 
+    # Return all of the info that we know how to provide.
+    # Return is in the form of a hash. Keys are the names of the info methods,
+    # values are the return values of said methods.
     def all_info
       return {} if !defined? @info_providers
 
